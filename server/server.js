b56f670827d9ca16de56767b75232f8a1198846f
yinzhedy <yinzhe.dy@gmail.com>
2021-11-21 17:43:54 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -22,8 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+// app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
