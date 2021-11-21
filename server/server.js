384a9af2d0649d9706243f991f6868cf8d46d1f0
yinzhedy <yinzhe.dy@gmail.com>
2021-11-21 16:53:40 -0500

    changed route on server.js for idex html to be served

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.static("public"));
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
